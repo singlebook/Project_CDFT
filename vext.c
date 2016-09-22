dbbf07d05bae7e18d7c21e8e964f42d3d177ffbf
Wei CHEN <wchen01@qq.com>
2016-09-22 13:38:41 +0800

    Add files via upload

--- vext.c
+++ vext.c
@@ -4,13 +4,35 @@
 void SetVext(){
 	
 	int loop;
+	int i,j,k;
+	real rr;
 	/*I set here the hard wall potential as a test.
 	 * The hard wall is put at the bottom of the simulation box.*/
-	
+	 
+/*  //Hard Wall	
 	for(loop=0;loop<VProd(Pts);loop++){
-		if(loop%Pts.z == 0) Vext[loop] = 0.0;
+		if(loop%Pts.z == 0) Vext[loop] = 100.0;
 		else Vext[loop] = 0.0; 
-		}
+   }
+*/
+    // LJ 12-6 Wall
+	for(k=0;k<Pts.z;k++){
+		rr = Sqr(k*Delta.z);
+		for(j=0;j<Pts.y;j++){
+			for(i=0;i<Pts.x;i++){
+				if( rr > Sqr(Rcut)){
+					Vext[k+Pts.z*(j+Pts.y*i)] = 0.0;
+                  }
+		       else if(rr <= Sqr(Rcut) && rr>0.0){
+					Vext[k+Pts.z*(j+Pts.y*i)] = 4.0 * Atom[0].epslion * (pow(Atom[0].sigma, 12)/pow(rr, 6) - pow(Atom[0].sigma, 6)/pow(rr, 3));
+					} 
+			   else if(rr == 0.0){
+					Vext[k+Pts.z*(j+Pts.y*i)] = 1000.0;
+					}						
+				}
+			}
+	}
+	
 	printf("Setting the external potential is done\n");
 	return;
 	}
@@ -19,7 +41,7 @@ void SetVext(){
 void SetInitialDensity(){
 	int loop;
 	for(loop=0;loop<VProd(Pts);loop++){
-        Density[loop] = 0.8*Atom[0].density /* exp(Atom[0].miu - Beta * Vext[loop])*/;
+        Density[loop] = 1.1*Atom[0].density;
 		}
 	printf("Setting the initial density is done\n");
 	return;
